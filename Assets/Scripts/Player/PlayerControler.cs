f57c2640e0a102c38d9cff9b7c87d3b11ced7f7e
felicien-golfier <feloux@gmail.com>
2022-07-26 16:41:24 +0200

    Player Authority

--- Assets/Scripts/Player/PlayerControler.cs
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Unity.Netcode;
 using System;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerControler : NetworkBehaviour
 {
     public float speed = 15.0f;
     private VJHandler Joystick;
@@ -54,15 +54,40 @@ public class PlayerControler : MonoBehaviour
 
     private void UpdateTransform()
     {
-
-
         Vector3 oldPos = transform.position;
         float frameSpeed = Time.deltaTime * speed;
         Vector3 frameTranslation = new Vector3(frameSpeed * horizontalInput, frameSpeed * verticalInput, 0);
         float acos = Mathf.Acos(frameTranslation.normalized.x);
         float sign = Mathf.Sign(Mathf.Sin(frameTranslation.normalized.y));
         float radAngle = acos * sign;
-        transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * acos * sign -90);
+        float rotation = Mathf.Rad2Deg * acos * sign - 90;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation);
         transform.position = transform.position + frameTranslation;
+
+        if (IsHost)
+        {
+            UpdatePosClientRpc(new Vector2(transform.position.x, transform.position.y), rotation);
+        }
+        else
+        {
+            UpdatePosServerRpc(new Vector2(transform.position.x, transform.position.y), rotation);
+        }
     }
-}
+
+    [ServerRpc]
+    public void UpdatePosServerRpc(Vector2 Position, float Orientation)
+    {
+        //Transfert position to other players
+        UpdatePosClientRpc(Position, Orientation);
+    }
+
+    [ClientRpc]
+    public void UpdatePosClientRpc(Vector2 Position, float Orientation)
+    {
+        if (IsLocalPlayer)
+            return;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, Orientation);
+        transform.position = Position;
+    }
+};
